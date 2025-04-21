6aca831e99ca7abd5d97d2e1bbfc6467dcb63e4c
Evgeny Kniazev <eugene.kniazev@gmail.com>
2025-04-21 12:53:38 +0200

    upd: add react to preact alias

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), preact()],
+  resolve: {
+    alias: {
+      react: "preact/compat",
+      "react-dom": "preact/compat",
+    },
+  },
 });
